b4d80d7375188f375c80dc5b097cbd6759dabfe8
WilliamTuominiemi <william.m.tuominiemi@gmail.com>
2025-12-28 16:43:58 +0200

    event struct

---
+++ src/event.rs
@@ -0,0 +1,34 @@
+use crate::event_type::EventType;
+
+use std::time::SystemTime;
+
+pub struct Event {
+    event_type: EventType,
+    timestamp: SystemTime,
+    payload: String,
+}
+
+impl Event {
+    pub fn new(event_type: EventType, payload: String) -> Self {
+        let timestamp = SystemTime::now();
+
+        Event {
+            event_type,
+            timestamp,
+            payload,
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_initialize() {
+        let event = Event::new(EventType::A, "test".to_string());
+
+        assert_eq!(event.event_type, EventType::A);
+        assert_eq!(event.payload, "test");
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
+use event_type::EventType;
+mod event_type;
+
+mod event;
+use event::Event;
+
 fn main() {
     println!("Hello, world!");
 }
